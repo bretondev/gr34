9117568bdcd02f223f32fa6c576fb4390af1dffa
bretondev <33033418+bretondev@users.noreply.github.com>
2022-06-02 19:31:58 +0200

    Commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
     loadNavigationBar();
     loadPhotos();
+    setSidebarLogic();
 });
 
 function loadNavigationBar() {
@@ -12,4 +13,35 @@ function loadPhotos() {
     for (let photo of photos) {
         photo.src = "https://gr34.s3.eu-west-3.amazonaws.com/" + photo.dataset.url;
     };
+}
+
+function setSidebarLogic() {
+    // Hide submenus
+    $('#sidebar .collapse').collapse('hide'); 
+
+    // Collapse/Expand icon
+    $('#collapse-icon').addClass('fa-angle-double-left'); 
+
+    // Collapse click
+    $('[data-toggle=sidebar-colapse]').click(function() {
+        SidebarCollapse();
+    });
+
+    function SidebarCollapse () {
+        $('.menu-collapsed').toggleClass('d-none');
+        $('.sidebar-submenu').toggleClass('d-none');
+        $('.submenu-icon').toggleClass('d-none');
+        $('#sidebar-container').toggleClass('sidebar-expanded sidebar-collapsed');
+        
+        // Treating d-flex/d-none on separators with title
+        var SeparatorTitle = $('.sidebar-separator-title');
+        if ( SeparatorTitle.hasClass('d-flex') ) {
+            SeparatorTitle.removeClass('d-flex');
+        } else {
+            SeparatorTitle.addClass('d-flex');
+        }
+        
+        // Collapse/Expand icon
+        $('#collapse-icon').toggleClass('fa-angle-double-left fa-angle-double-right');
+    }
 }
