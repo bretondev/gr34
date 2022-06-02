5ad0997f1b946f00cd6e3dad892b85bd804b07a7
bretondev <33033418+bretondev@users.noreply.github.com>
2022-06-02 03:12:40 +0200

    Commit

--- chapters/script.js
+++
@@ -1,7 +0,0 @@
-
-
-var photos = document.getElementsByTagName('img');
-
-for (let photo of photos) {
-    photo.src = "https://gr34.s3.eu-west-3.amazonaws.com/" + photo.dataset.url;
-};
---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+
+
+
+
+$(document).ready(function() {
+    var photos = document.getElementsByTagName('img');
+
+    for (let photo of photos) {
+        photo.src = "https://gr34.s3.eu-west-3.amazonaws.com/" + photo.dataset.url;
+    };
+});
